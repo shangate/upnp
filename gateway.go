c2aa609d83deb4272ab0223c2a69489f675c60ca
zzx <zzx@shangate.com>
2023-05-14 10:53:06 +0800

    update

--- gateway.go
+++ gateway.go
@@ -27,14 +27,12 @@ func (gateway *Gateway) Send() bool {
 		"HOST: 239.255.255.250:1900\r\n" +
 		"ST: urn:schemas-upnp-org:service:WANIPConnection:1\r\n" +
 		"MAN: \"ssdp:discover\"\r\n" + "MX: 3\r\n\r\n"
-	c := make(chan string)
-	go gateway.send(message, c)
-	result := <-c
-	if result == "" {
+	result, err := gateway.send(message)
+	if err != nil {
 		gateway.Active = false
 		return false
 	}
-	err := gateway.resolve(result)
+	err = gateway.resolve(result)
 	if err != nil {
 		return false
 	}
@@ -44,51 +42,43 @@ func (gateway *Gateway) Send() bool {
 	return true
 }
 
-func (gateway *Gateway) send(message string, c chan string) error {
-	var conn *net.UDPConn
-	defer func() {
-		if r := recover(); r != nil {
-			//timeout
-		}
-	}()
-	go func(conn *net.UDPConn) {
-		defer func() {
-			if r := recover(); r != nil {
-				//doesn't timeout
-			}
-		}()
-		time.Sleep(time.Second)
-		c <- ""
-		conn.Close()
-	}(conn)
-	remoteAddr, err := net.ResolveUDPAddr("udp", "239.255.255.250:1900")
+func (gateway *Gateway) send(message string) (result string, err error) {
+	remoteAddr, err := net.ResolveUDPAddr("udp4", "239.255.255.250:1900")
 	if err != nil {
-		return fmt.Errorf("resolve udp addr error %v", err)
+		return result, fmt.Errorf("resolve udp addr error %v", err)
 	}
-	localAddr, err := net.ResolveUDPAddr("udp", ":")
+
+	conn, err := net.ListenUDP("udp4", &net.UDPAddr{IP: net.IPv4zero, Port: 0})
 	if err != nil {
-		return fmt.Errorf("resolve udp addr error %v", err)
-	}
-	conn, err = net.ListenUDP("udp", localAddr)
-	if err != nil {
-		return fmt.Errorf("listen udp error %v", err)
+		return result, fmt.Errorf("listen udp error %v", err)
 	}
 	defer conn.Close()
+
 	_, err = conn.WriteToUDP([]byte(message), remoteAddr)
 	if err != nil {
-		return fmt.Errorf("send message error %v", err)
+		fmt.Println("send message error ", err)
+		return result, fmt.Errorf("send message error %v", err)
 	}
+
 	buf := make([]byte, 1024)
+	err = conn.SetReadDeadline(time.Now().Add(time.Second * 3))
+	if err != nil {
+		fmt.Println("set read deadline error ", err)
+		return result, fmt.Errorf("set read deadline error %v", err)
+	}
 	n, addr, err := conn.ReadFromUDP(buf)
 	if err != nil {
-		return fmt.Errorf("recv message error %v", err)
+		fmt.Println("recv message error ", err)
+		return result, fmt.Errorf("recv message error %v", err)
 	}
-	gatewayAddr, _ := net.ResolveUDPAddr("udp", addr.String())
+
+	gatewayAddr, _ := net.ResolveUDPAddr("udp4", addr.String())
 	gateway.LocalAddr = gatewayAddr.IP.String()
 
-	result := string(buf[:n])
-	c <- result
-	return nil
+	conn.SetReadDeadline(time.Time{})
+
+	result = string(buf[:n])
+	return result, nil
 }
 
 func (gateway *Gateway) resolve(result string) error {
